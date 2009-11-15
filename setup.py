012350887a55f69e415074f79bc66b3dba18a1eb
mh5 <devnull@localhost>
2009-11-15 22:37:40 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ name, short_description = doclines[0].split(": ")
 long_description = "\n".join(doclines[2:])
 
 url = "http://www.ebi.ac.uk/~hoffman/software/%s/" % name.lower()
-download_url = "%s/%s-%s.tar.gz" % (url, name, __version__)
+download_url = "%s%s-%s.tar.gz" % (url, name, __version__)
 
 classifiers = ["License :: OSI Approved :: GNU General Public License (GPL)",
                "Natural Language :: English",
@@ -40,7 +40,7 @@ setup(name=name,
       author_email="hoffman+%s@ebi.ac.uk" % name.lower(),
       url=url,
       download_url=download_url,
-      license="GNU GPL",
+      license="GNU GPLv2",
       classifiers=classifiers,
       long_description = long_description,
       package_dir = {'': 'lib'},
