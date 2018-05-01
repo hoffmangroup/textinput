72ca81f4fda467dc7d3cb0f80132174dea9359ea
Eric Roberts <eroberts@uhnresearch.ca>
2018-05-01 16:25:46 -0400

    Update version number and package metadata

--- setup.py
+++ setup.py
@@ -16,16 +16,13 @@ also allowed.
 """
 
 from __future__ import absolute_import
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 from setuptools import find_packages, setup
 
 doclines = __doc__.splitlines()
 name, short_description = doclines[0].split(": ")
 long_description = "\n".join(doclines[2:])
-
-url = "http://noble.gs.washington.edu/~mmh1/software/%s/" % name.lower()
-download_url = "%s%s-%s.tar.gz" % (url, name, __version__)
 
 classifiers = ["License :: OSI Approved :: GNU General Public License (GPL)",
                "Natural Language :: English",
@@ -34,10 +31,8 @@ classifiers = ["License :: OSI Approved :: GNU General Public License (GPL)",
 setup(name=name,
       version=__version__,
       description=short_description,
-      author="Michael Hoffman",
-      author_email="mmh1@uw.edu",
-      url=url,
-      download_url=download_url,
+        author="Michael Hoffman",
+        author_email="michael.hoffman@utoronto.ca",
       license="GNU GPLv2",
       classifiers=classifiers,
       long_description = long_description,
