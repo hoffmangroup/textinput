6c12427ad6d40402893100966a17c0aae6cc3a3c
Marc Asenjo <markasenjo@hotmail.com>
2018-05-01 15:29:57 -0400

    modernized test_tabdelim.py

--- test/test_tabdelim.py
+++ test/test_tabdelim.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import
 __version__ = "$Revision: 1.1 $"
 
 import unittest
@@ -11,18 +12,18 @@ class TestDictReader(unittest.TestCase):
         lines =['JOBID\tARRAY_SPEC',
                 '266915\ti.blastz.mbe[1-1000]%200']
         reader = tabdelim.DictReader(lines)
-        jobinfo = reader.next()
+        jobinfo = next(reader)
         self.assertEqual(jobinfo["ARRAY_SPEC"], "i.blastz.mbe[1-1000]%200")
 
         reader = tabdelim.DictReader(lines, skipinitialspace=True)
-        jobinfo = reader.next()
+        jobinfo = next(reader)
         self.assertEqual(jobinfo["ARRAY_SPEC"], "i.blastz.mbe[1-1000]%200")
 
     def test_untabified(self):
         lines =['JOBID    ARRAY_SPEC',
                 '266915   i.blastz.mbe[1-1000]%200']
         reader = tabdelim.DictReader(lines, delimiter=" ", skipinitialspace=True)
-        jobinfo = reader.next()
+        jobinfo = next(reader)
         self.assertEqual(jobinfo["ARRAY_SPEC"], "i.blastz.mbe[1-1000]%200")
 
 if __name__ == "__main__":
